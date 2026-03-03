78a442dcb6db18de256e4cc6065f981855d7cc96
Sherif-toiema22 <sherif.toiema22@gmail.com>
2026-03-03 23:38:17 +0200

    Sting test

--- utilis.test.js
+++ utilis.test.js
@@ -1,6 +1,11 @@
-const {sum} = require('./utilis');
+const {sum,greeting} = require('./utilis');
 
 test('sum - should return 2 + 3 = 5', () => {
     const result = sum (2,3);
     expect(result).toBe(5);
+});
+
+test('greeting - should return Hello mahmoud', () => {
+    expect(greeting ('mahmoud')).toMatch('Hello mahmoud');
+
 });
